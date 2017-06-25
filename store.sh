6f05a91ab6935842e9563d2d8834244c6459aa43
Andrey Kuzmin <prepodam@mail.ru>
2017-06-25 18:27:10 +0300

    Reload if certificates changed.

--- store.sh
+++ store.sh
@@ -2,6 +2,19 @@
 
 dir=$(dirname $1)
 mkdir -p $dir
-echo "$2" > $1
-chmod 600 $1
+
+echo "$2" > "$1.tmp"
+
+touch "$1"
+diff=$(diff "$1" "$1.tmp")
+
+if [ -n "$diff" ]; then
+  mv "$1.tmp" "$1"
+  chmod 600 "$1"
+
+  nginx -s reload
+else
+  rm "$1.tmp"
+fi
+
 exit 0
